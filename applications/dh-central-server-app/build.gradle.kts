c94b57b6d83578c9a3cb3314acfe0b1b869fbc06
Andrey Likarovski <likarovski@gmail.com>
2019-10-16 18:14:40 +0700

    Скрипты для сборки в gitlab

--- applications/dh-central-server-app/build.gradle.kts
+++ applications/dh-central-server-app/build.gradle.kts
@@ -2,6 +2,9 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 dependencies {
 
+    api("org.springframework.boot:spring-boot-starter-web")
+
+
     implementation(project(":service:data-storage-service:data-storage-service-api"))
     implementation(project(":service:queue-manager-service:queue-manager-service-api"))
     implementation(project(":service:location-service:location-service-api"))
@@ -13,4 +16,8 @@ dependencies {
     runtime(project(":service:location-service:location-service-impl"))
     runtime(project(":service:mis-integration-service:mis-integration-service-impl"))
     runtime(project(":service:mis-integration-service:mis-integration-service-rest"))
+}
+
+tasks.withType<BootJar>() {
+    launchScript()
 }
--- service/mis-integration-service/mis-integration-service-rest/build.gradle.kts
+++ service/mis-integration-service/mis-integration-service-rest/build.gradle.kts
@@ -2,5 +2,5 @@
 
 dependencies {
     api(project(":service:mis-integration-service:mis-integration-service-api"))
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    api("org.springframework.boot:spring-boot-starter-web")
 }
